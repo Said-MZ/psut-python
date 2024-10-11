063a265ecfeaa39c2365cab798e2738a07be12eb
Said-MZ <saidzain897@gmail.com>
2024-10-11 21:53:21 +0300

    fix playground.py to be at the project's root

---
+++ playground.py
@@ -0,0 +1,9 @@
+# playground.py
+
+print("Welcome to the Python Playground!")
+
+def experiment():
+    print("This is where you can try out new ideas!")
+
+if __name__ == "__main__":
+    experiment()
