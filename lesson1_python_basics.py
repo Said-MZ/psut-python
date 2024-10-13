cc84d3b37e7b95bac9b3f5735f5aa0e714c5043d
said zain <saidzain897@gmail.com>
2024-10-13 11:52:24 +0300

    print statement

---
+++ lesson1_python_basics.py
@@ -0,0 +1,21 @@
+# 10 - 13 - 2024
+# Chapter 2
+
+# Printing in Python
+
+# We Can use double quotations " "
+print("Hello, World!")
+
+# We Can also use single quotations ' '
+print('Hello, World!')
+
+# But what if the string includes single quotation/s ?
+# We use double quotation for strings with single quotations:
+print("Don't study")
+
+# But what if the string includes double quotation/s ?
+# We use single quotation for strings with double quotations:
+print('This is a quote: "Quote"')
+
+# We can also use triple (double quotations) """ """
+print("""Something""")
