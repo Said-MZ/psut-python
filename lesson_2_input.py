728ce8c4da9b8695c89b3597765a403356610ef0
said zain <saidzain897@gmail.com>
2024-10-15 11:46:32 +0300

    Add input and variables

---
+++ lesson_2_input.py
@@ -0,0 +1,13 @@
+# 10 - 15 - 2024
+# Chapter 2
+
+# Input
+
+"""
+name = input("What's your name")
+print("Hello, ", name)
+age = int(input("How old are you?")) # age will be an int here
+print("You are, ", age ,"years old")
+salary = float(input("What's your salary?")) # salary will be a float here
+print("Your salary is", salary)
+"""
---
+++ lesson_2_variables.py
@@ -0,0 +1,54 @@
+# 10 - 15 - 2024
+# Chapter 2
+
+# Variables
+
+# Naming Rules
+# 1. Not a reserved Keyword like: (while, if, elif..., etc.)
+# 2. First Char must be an _ or a letter
+# 3. Variables names are case-sensitive
+# Note: Variable names should reflect its use
+# Note: Variable names should be in snake_case
+
+# Example:
+
+"""
+width=10
+length=6
+
+print("width:", width)
+print("length: ", length)
+"""
+
+# -----------------------------------------------------------------------------
+
+# Variable Reassignment
+
+"""
+dollars = 2.76
+print("Today i have ", dollars, " Dollars in my account!")
+dollars = 3.76
+print("But now i have ", dollars, " Dollars in my account!")
+"""
+
+# -----------------------------------------------------------------------------
+
+# Data types
+"""
+name="ahmad"
+age=20
+salary=900.501
+
+# the type function shows the data type of the variable
+print(type(name))
+print(type(age))
+print(type(salary))
+"""
+
+
+# Auto type casting
+"""
+x = "PSUT"  # string
+
+x=8  # Now it is an int
+"""
